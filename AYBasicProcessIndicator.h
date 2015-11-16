512e582b19f929bbd7fd85303390922acbecfc1f
ArthurYV <yangjingbing007@gmail.com>
2015-11-16 10:20:00 +0800

    modify

---
+++ AYBasicProcessIndicator.h
@@ -0,0 +1,13 @@
+//
+//  AYBasicProcessIndicator.h
+//  AYProcessView
+//
+//  Created by Arthury on 15/11/16.
+//  Copyright © 2015年 Arthury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AYBasicProcessIndicator : UIView
+
+@end
---
+++ AYBasicProcessIndicator.m
@@ -0,0 +1,14 @@
+//
+//  AYBasicProcessIndicator.m
+//  AYProcessView
+//
+//  Created by Arthury on 15/11/16.
+//  Copyright © 2015年 Arthury. All rights reserved.
+//
+
+#import "AYBasicProcessIndicator.h"
+
+@implementation AYBasicProcessIndicator
+
+
+@end
